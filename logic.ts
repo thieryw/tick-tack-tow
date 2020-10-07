ee3097b26ca3cacaf4dcc341fa0165f3252ad2c8
thieryw <williamalexthiery@hotmail.fr>
2020-10-07 18:38:50 +0200

    1 file modified

--- logic.ts
+++ logic.ts
@@ -1,282 +1,147 @@
 import { Evt, NonPostableEvt, ToPostableEvt } from "evt";
 
-type Mark = "x" | "o";
+export type Coordinate = 1 | 2 | 3;
 
-type Coordinate = 1 | 2 | 3;
-
-type Coordinates = {
+export type Coordinates = {
   x: Coordinate;
   y: Coordinate;
+
 }
 
-type Box = {
-  mark: Mark | undefined;
-  coordinates: Coordinates;
-}
+type Mark = "x" | "o";
 
 
-
-export type Store = Readonly<{
-
-  boxes: [Box, Box, Box,
-          Box, Box, Box,
-          Box, Box, Box];
+export type Store = {
+  boxes: [
+          [Mark | undefined, Mark | undefined, Mark | undefined],
+          [Mark | undefined, Mark | undefined, Mark | undefined],
+          [Mark | undefined, Mark | undefined, Mark | undefined]
+         ];
 
   getMarkAtCoordinates: (coordinates: Coordinates) => Mark | undefined;
   currentPlayerMark: Mark;
-  play: (params: {coordinates: Coordinates; mark: Mark})=> Promise<void>;
-  gameStatus: {
-    isGameWon: boolean;
-    hasGameStarted: boolean;
-    winnerMark: Mark | undefined;
-  };
-
+  play: (params: {coordinates: Coordinates; mark: Mark;}) => Promise<void>;
   newGame: ()=> Promise<void>;
 
-
-  evtGameWon: NonPostableEvt<Store["gameStatus"]>
   evtPlayed: NonPostableEvt<Parameters<Store["play"]>[0]>;
-  evtGameRestarted: NonPostableEvt<Box[]>;
+  evtGameRestarted: NonPostableEvt<{boxes: Store["boxes"]; currentPlayerMark: Store["currentPlayerMark"]}>;
+}
+
+type StoreLike = Pick<Store, "getMarkAtCoordinates">;
+
+export function isGameWon(store: StoreLike): boolean{
+
+  const getMarkCoef = (mark: Mark)=> mark === "o" ? -1 : 1;
+
+  const isWining = (m1: Mark, m2: Mark, m3: Mark)=>
+    Math.abs(getMarkCoef(m1) + getMarkCoef(m2) + getMarkCoef(m3)) === 3;
 
 
+  for(const x of [1, 2, 3] as const){
+    if(isWining(
+      store.getMarkAtCoordinates({x, "y": 1}),
+      store.getMarkAtCoordinates({x, "y": 2}),
+      store.getMarkAtCoordinates({x, "y": 3})
+    )){
+      return true;
+    }
+  }
 
-  
+  for(const y of [1, 2, 3] as const){
+    if(isWining(
+      store.getMarkAtCoordinates({"x": 1, y}),
+      store.getMarkAtCoordinates({"x": 2, y}),
+      store.getMarkAtCoordinates({"x": 3, y})
+    )){
+      return true;
 
-}>
+    }
+  }
+
+  if(isWining(
+    store.getMarkAtCoordinates({"x": 1, "y": 1}),
+    store.getMarkAtCoordinates({"x": 2, "y": 2}),
+    store.getMarkAtCoordinates({"x": 3, "y": 3})
+  )){
+    return true;
+  }
+
+  if(isWining(
+    store.getMarkAtCoordinates({"x": 1, "y": 3}),
+    store.getMarkAtCoordinates({"x": 2, "y": 2}),
+    store.getMarkAtCoordinates({"x": 3, "y": 1})
+  )){
+    return true;
+  }
+
+
+  return false;
+}
 
 
 
 export async function getStore(): Promise<Store>{
-  
-  let boxes: Store["boxes"] = [
-    {
-      "coordinates": {"x": 1, "y": 1},
-      "mark": undefined,
-    },
-    {
-      "coordinates": {"x": 2, "y": 1},
-      "mark": undefined,
-    },
-    {
-      "coordinates": {"x": 3, "y": 1},
-      "mark": undefined,
-    },
-    {
-      "coordinates": {"x": 1, "y": 2},
-      "mark": undefined,
-    },
-    {
-      "coordinates": {"x": 2, "y": 2},
-      "mark": undefined,
-    },
-    {
-      "coordinates": {"x": 3, "y": 2},
-      "mark": undefined,
-    },
-    {
-      "coordinates": {"x": 1, "y": 3},
-      "mark": undefined,
-    },
-    {
-      "coordinates": {"x": 2, "y": 3},
-      "mark": undefined,
-    },
-    {
-      "coordinates": {"x": 3, "y": 3},
-      "mark": undefined,
-    },
 
-  ];
-
-
-  const simulateNetworkDelay = (ms: number)=>{
-    return new Promise<void>(resolve=> setTimeout(resolve, ms));
+  const simulateNetworkDelay = (delay: number)=>{
+    return new Promise<void>(resolve => setTimeout(resolve, delay));
   }
- 
-
-  
-
-  
-  
-
-  const getGameStatus = (boxes: Store["boxes"]): Store["gameStatus"] => {
-    
-    const listOfOBoxes: Box[] = [];
-    const listOfXBoxes: Box[] = [];
-    
-    boxes.forEach(box=>{
-      switch(box.mark){
-        case "o" as Mark : listOfOBoxes.push(box); return;
-        case "x" as Mark : listOfXBoxes.push(box); return;
-        case undefined : return; 
-      }
-    })
-
-   
-
-    for(const list of [listOfOBoxes, listOfXBoxes]){
-      if(list.length >= 3){
-        let numberOfboxesWithSameXCoord = 0;
-        let numberOfboxesWithSameYCoord = 0;
-        let diagonalBoxesBottomToTop = 0;
-        let diagonalBoxesTopToBottom = 0;
-        
-        for(let i = 0; i < list.length - 1; i++){
-          for(let j = i+1; j < list.length; j++){
-            
-            if(list[i].coordinates.x === list[j].coordinates.x){
-              numberOfboxesWithSameXCoord++;                           
-            }
-
-            if(list[i].coordinates.y === list[j].coordinates.y){
-              numberOfboxesWithSameYCoord++;             
-            }
-            
-
-            if(numberOfboxesWithSameYCoord === 3 || numberOfboxesWithSameXCoord === 3){
-              return {
-                "isGameWon": true,
-                "hasGameStarted": true,
-                "winnerMark": list[j].mark,
-              }
-            }
-
-
-          }
-        }
-
-        for(const box of list){
-          if(box.coordinates.x === 1 && box.coordinates.y === 3 ||
-            box.coordinates.x === 3 && box.coordinates.y === 1){
-            diagonalBoxesBottomToTop++;
-          }
-
-          if(box.coordinates.x === box.coordinates.y){
-            diagonalBoxesTopToBottom++;
-            if(box.coordinates.x === 2){
-              diagonalBoxesBottomToTop++;
-            }
-          }
-
-          if(diagonalBoxesTopToBottom === 3 || diagonalBoxesBottomToTop === 3){
-            return {
-              "isGameWon": true,
-              "hasGameStarted": true,
-              "winnerMark": box.mark,
-            }
-          }
-          
-        }
-
-      }
-    }
-
-    return {
-      "isGameWon": false,
-      "hasGameStarted": true,
-      "winnerMark": undefined,
-    }
-    
-  }
-
 
   const store: ToPostableEvt<Store> = {
-    boxes,
-    "getMarkAtCoordinates": coordinates =>{
-      let mark: Mark;
-      boxes.forEach(box =>{
-        if(box.coordinates === coordinates){
-          mark = box.mark;
-          return;
-        }
-      });
-
-      return mark;
-      
-    },
+    "boxes": [
+      [undefined, undefined, undefined],
+      [undefined, undefined, undefined],
+      [undefined, undefined, undefined]
+    ],
 
     "currentPlayerMark": "o",
-
-    "gameStatus": {
-      "hasGameStarted": false,
-      "isGameWon": false,
-      "winnerMark": undefined,
+    "getMarkAtCoordinates": coordinates =>{
+      return store.boxes[coordinates.x - 1][coordinates.y - 1];
     },
 
-    "evtPlayed": new Evt(),
-    "evtGameWon": new Evt(),
-    
     "play": async params =>{
-      
-      
-      
-      if(store.gameStatus.isGameWon){       
-        return;
+
+      if(isGameWon(store)){
+        throw Error("Error game finiched, can not play!");
       }
 
       await simulateNetworkDelay(300);
 
-      boxes.forEach((box, index)=>{
-        if(box.coordinates === params.coordinates && box.mark === undefined){
-          boxes[index].mark = store.currentPlayerMark;
-          (store.currentPlayerMark as Store["currentPlayerMark"]) = store.currentPlayerMark === "o" 
-          ? "x" : "o";
-          
-          return;
-        }
-      });
+      store.boxes[params.coordinates.x - 1][params.coordinates.y - 1] = params.mark;
+      store.currentPlayerMark = store.currentPlayerMark === "o" ? "x" : "o";
 
-    
-      (store.gameStatus as Store["gameStatus"]) = getGameStatus(boxes);
-
-      if(store.gameStatus.isGameWon){
-        store.evtGameWon.post(store.gameStatus);
-      }
-      
       store.evtPlayed.post(params);
-      
+
     },
 
     "newGame": async ()=>{
-      if(!store.gameStatus.hasGameStarted){
-        return;
-      }
-      await simulateNetworkDelay(500);
+      await simulateNetworkDelay(300);
 
-      boxes.forEach(box =>{
-        box.mark = undefined;
+
+      store.boxes.forEach(box =>{
+        box.forEach(mark =>{
+          if(mark === undefined){
+            return;
+          }
+
+          mark = undefined;
+        });
       });
 
-      (store.currentPlayerMark as Store["currentPlayerMark"]) = "o";
-      (store.gameStatus as Store["gameStatus"]) = {
-        "isGameWon": false,
-        "hasGameStarted": false,
-        "winnerMark": undefined,
-      };
+
+      store.currentPlayerMark = "o";
+
+      store.evtGameRestarted.post({"boxes": store.boxes, "currentPlayerMark": store.currentPlayerMark});
       
-
-
-      store.evtGameRestarted.post(boxes);
-
+      
     },
 
-
+    "evtPlayed": new Evt(),
     "evtGameRestarted": new Evt(),
-
-    
-
-
-
   }
 
-  await simulateNetworkDelay(2000);
+  await simulateNetworkDelay(1500);
 
   return store;
-
-
-
-  
-
 }
 
 
